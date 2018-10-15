2c1eb252635ba85eabd6394ad2bbc599e7e95f8a
Dnl <daniel.souza@eleevait.com.br>
2018-10-14 22:19:04 -0300

    Pessoas e lancamentos

--- src/app/lancamentos/lancamentos-novo/lancamentos-novo.component.ts
+++ src/app/lancamentos/lancamentos-novo/lancamentos-novo.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CategoriasService} from '../../categorias/categorias.service';
+import {ErrorHandlerService} from '../../core/error-handler.service';
+import {PessoasFiltro, PessoasService} from '../../pessoas/pessoas.service';
 
 @Component({
   selector: 'app-lancamentos-novo',
@@ -7,27 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosNovoComponent implements OnInit {
 
+  constructor(private categoriasService: CategoriasService,
+              private pessoaService: PessoasService,
+              private errorHandler: ErrorHandlerService) {
+  }
+
   tipos = [
     {label: 'Receita', value: 'RECEITA'},
     {label: 'Despesa', value: 'DESPESA'}
   ];
 
-  categorias = [
-    {label: 'Alimentação', value: 1},
-    {label: 'Transporte', value: 2}
-  ];
+  categorias = [];
+  pessoas = [];
 
 
-  pessoas = [
-    {label: 'João da silva', value: 1},
-    {label: 'Sebastiao de souza', value: 2},
-    {label: 'Daniel souza', value: 3}
-  ];
-
-
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.pesquisarCategorias();
+    this.pesquisarPessoas();
   }
 
+  pesquisarCategorias() {
+    this.categoriasService.pesquisar()
+      .then(categorias => {
+        this.categorias = categorias.map(categoria => {
+          return {label: categoria.nome, value: categoria.codigo};
+        });
+      })
+      .catch(error => this.errorHandler.handle(error));
+  }
+
+  pesquisarPessoas() {
+    this.pessoaService.pesquisar( {nome: undefined, size : 0, page: 0})
+      .then(result => {
+        this.pessoas = result.pessoas.map(pessoa => {
+          return {label: pessoa.nome, value: pessoa.codigo};
+        });
+      })
+      .catch(error => this.errorHandler.handle(error));
+  }
 }
