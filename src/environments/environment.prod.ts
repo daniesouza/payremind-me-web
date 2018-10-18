42fb38ae29555d7a919383c638be1c0b8cd6c34c
Dnl <daniel.souza@eleevait.com.br>
2018-10-18 06:50:08 -0300

    seguranca

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: 'https://payremind-me-api.herokuapp.com',
 
   tokenWhitelistedDomains: [ new RegExp('payremind-me-api.herokuapp.com') ],
-  tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token/') ]
+  tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 
   // tokenWhitelistedDomains: [ 'payremind-me-api.herokuapp.com' ],
   // tokenBlacklistedRoutes: [ 'payremind-me-api.herokuapp.com/oauth/token/' ]
