05c6b120be7195c1df242d3be8a6f9b459fd47f5
Dnl <daniel.souza@eleevait.com.br>
2018-10-26 09:54:26 -0300

    versao 1.0 success

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,14 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://ec2-18-231-91-14.sa-east-1.compute.amazonaws.com:8080',
+//   whitelistedDomains: ['ec2-18-231-91-14.sa-east-1.compute.amazonaws.com:8080' ],
+//   blacklistedRoutes: [ 'http://ec2-18-231-91-14.sa-east-1.compute.amazonaws.com:8080/oauth/token' ]
+// };
+
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-18-231-91-14.sa-east-1.compute.amazonaws.com:8080',
-  whitelistedDomains: ['ec2-18-231-91-14.sa-east-1.compute.amazonaws.com:8080' ],
-  blacklistedRoutes: [ 'http://ec2-18-231-91-14.sa-east-1.compute.amazonaws.com:8080/oauth/token' ]
+  apiUrl: 'https://payremind-me-api.herokuapp.com',
+  whitelistedDomains: ['payremind-me-api.herokuapp.com'],
+  blacklistedRoutes: ['https://payremind-me-api.herokuapp.com/oauth/token/']
 };
+
