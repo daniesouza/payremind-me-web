43e6a4de1f7dc6fdbdba9cbf68fa6f9aba24b5c2
Dnl <daniel.souza@eleevait.com.br>
2018-10-26 10:01:37 -0300

    versao 1.0 success

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,6 +9,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://payremind-me-api.herokuapp.com',
   whitelistedDomains: ['payremind-me-api.herokuapp.com'],
-  blacklistedRoutes: ['https://payremind-me-api.herokuapp.com/oauth/token/']
+  blacklistedRoutes: ['https://payremind-me-api.herokuapp.com/oauth/token']
 };
 
